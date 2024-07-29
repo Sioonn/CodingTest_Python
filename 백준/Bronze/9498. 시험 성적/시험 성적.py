93bf50027bbabfe880086cc5a1c134eb74cf4f96
Lee-Sion <74344298+Sioonn@users.noreply.github.com>
2024-07-30 01:40:06 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+record = int(input())
+
+if 90<= record <= 100:
+    print("A")
+elif 80 <= record <= 89:
+    print("B")
+elif 70 <= record <= 79:
+    print("C")
+elif 60 <= record <= 69:
+    print("D")
+else:
+    print("F")
